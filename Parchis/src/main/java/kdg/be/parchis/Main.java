583c829f1d6dc22cabc499ededfe4d1563245b25
DanielGomes123 <120711500+DanielGomes123@users.noreply.github.com>
2023-02-10 11:36:12 +0100

    Add files via upload

---
+++ Parchis/src/main/java/kdg/be/parchis/Main.java
@@ -0,0 +1,34 @@
+package kdg.be.parchis;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import kdg.be.parchis.model.ParchisModel;
+import kdg.be.parchis.views.ParchisPresenter;
+import kdg.be.parchis.views.ParchisMenuView;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws FileNotFoundException {
+        ParchisModel model =
+                new ParchisModel();
+        ParchisMenuView menu =
+                new ParchisMenuView();
+        ParchisPresenter presenter =
+                new ParchisPresenter(model, menu);
+        primaryStage.setScene(new Scene(menu));
+        presenter.addWindowEventHandlers();
+        primaryStage.setTitle("Parchis");
+        primaryStage.getIcons().add(new Image(new FileInputStream("src\\main\\java\\kdg\\be\\parchis\\resources\\Icon.png")));
+        primaryStage.setWidth(1280);
+        primaryStage.setHeight(720);
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
---
+++ Parchis/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module kdg.be.parchis {
+    requires javafx.controls;
+    exports kdg.be.parchis;
+    exports kdg.be.parchis.views;
+    exports kdg.be.parchis.model;
+}
