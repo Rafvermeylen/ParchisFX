fec20e6d53b29341dd636a620ae866f85becf81f
Rafvermeylen <74359858+Rafvermeylen@users.noreply.github.com>
2023-02-10 11:42:40 +0100

    Add files via upload

---
+++ ParchisFX-full/Parchis/src/main/java/kdg/be/parchis/model/ParchisModel.java
@@ -0,0 +1,13 @@
+package kdg.be.parchis.model;
+
+public class ParchisModel {
+    // public static final constanten
+// private attributen
+    public ParchisModel() {
+// Constructor
+    }
+// implementatie logica van de
+// applicatie ahv methods
+// implementatie van de nodige Getters
+// implementatie van de nodige Setters
+}
---
+++ ParchisFX-full/Parchis/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module kdg.be.parchis {
+    requires javafx.controls;
+    requires javafx.media;
+
+    exports kdg.be.parchis;
+    exports kdg.be.parchis.views;
+    exports kdg.be.parchis.model;
+}
