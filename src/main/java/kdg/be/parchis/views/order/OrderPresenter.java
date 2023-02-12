18b84b8aeb5c7c1535a9d873859ea61fcf0758d0
Daniel Gomes <rui.gomesvieira@student.kdg.be>
2023-02-12 18:32:37 +0100

    Added dice roll in order.

--- src/main/java/kdg/be/parchis/views/order/OrderPresenter.java
+++ src/main/java/kdg/be/parchis/views/order/OrderPresenter.java
@@ -4,16 +4,20 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.media.AudioClip;
 import javafx.scene.media.Media;
+import kdg.be.parchis.model.game.Die;
 import kdg.be.parchis.model.menu.PlayerSetup;
 import kdg.be.parchis.views.playersetup.PlayerSetupView;
 
 import java.io.File;
 import java.util.Set;
 
+import static java.lang.Thread.sleep;
+
 public class OrderPresenter {
     private PlayerSetup setup;
     private OrderView view;
     private PlayerSetupView backView;
+    private int roll;
     public OrderPresenter(
             PlayerSetup model,
             OrderView view,
@@ -38,6 +42,37 @@ public class OrderPresenter {
                 AudioClip sound = new AudioClip(media.getSource());
                 sound.play();
                 view.getScene().setRoot(backView);
+            }
+        });
+        view.getRoll().setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                String path = "resources\\audio\\roll.wav";
+                Media media = new Media(new File(path).toURI().toString());
+                // Just media doesn't work, x.getSource has to be added as well.
+                AudioClip sound = new AudioClip(media.getSource());
+                    sound.play();
+                    view.getRoll().setDisable(true);
+                try {
+                    sleep(2 * 1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                view.getRoll().setDisable(false);
+                roll = Die.throwDie();
+                if (roll == 1){
+                    view.getDiceFoto().setImage(view.getDie1());
+                } else if (roll == 2){
+                    view.getDiceFoto().setImage(view.getDie2());
+                } else if (roll == 3){
+                    view.getDiceFoto().setImage(view.getDie3());
+                } else if (roll == 4){
+                    view.getDiceFoto().setImage(view.getDie4());
+                } else if (roll == 5){
+                    view.getDiceFoto().setImage(view.getDie5());
+                } else if (roll == 6){
+                    view.getDiceFoto().setImage(view.getDie6());
+                }
             }
         });
     }
--- src/main/java/kdg/be/parchis/views/order/OrderView.java
+++ src/main/java/kdg/be/parchis/views/order/OrderView.java
@@ -5,6 +5,7 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import kdg.be.parchis.model.menu.PlayerSetup;
 
@@ -20,6 +21,15 @@ public class OrderView extends BorderPane {
     private Label player4Name;
     private Button roll;
     private Button back;
+    private Image empty;
+    private Image die1;
+    private Image die2;
+    private Image die3;
+    private Image die4;
+    private Image die5;
+    private Image die6;
+    private ImageView diceFoto;
+
 
 
     public OrderView (PlayerSetup setup) throws FileNotFoundException {
@@ -35,6 +45,15 @@ public class OrderView extends BorderPane {
         player4Name = new Label(setup.getPlayers().get(3).getName());
         roll = new Button("Roll");
         back = new Button("Back");
+
+        empty = new Image(new FileInputStream("resources\\graphics\\die\\empty.png"));
+        die1 = new Image(new FileInputStream("resources\\graphics\\die\\dice_one.png"));
+        die2 = new Image(new FileInputStream("resources\\graphics\\die\\dice_two.png"));
+        die3 = new Image(new FileInputStream("resources\\graphics\\die\\dice_three.png"));
+        die4 = new Image(new FileInputStream("resources\\graphics\\die\\dice_four.png"));
+        die5 = new Image(new FileInputStream("resources\\graphics\\die\\dice_five.png"));
+        die6 = new Image(new FileInputStream("resources\\graphics\\die\\dice_six.png"));
+        diceFoto = new ImageView(empty);
     }
     private void layoutNodes() {
         VBox names = new VBox(20);
@@ -43,8 +62,11 @@ public class OrderView extends BorderPane {
         HBox layout = new HBox(30);
         layout.getChildren().addAll(names, roll);
         layout.setAlignment(Pos.CENTER);
+        this.setTop(diceFoto);
         this.setCenter(layout);
         this.setBottom(back);
+        BorderPane.setAlignment(diceFoto, Pos.TOP_CENTER);
+        BorderPane.setMargin(diceFoto, new Insets(30));
         BorderPane.setAlignment(layout, Pos.CENTER);
         this.setBackground(new Background(new BackgroundImage(background, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
 
@@ -54,5 +76,37 @@ public class OrderView extends BorderPane {
 
     public Button getBack() {
         return back;
+    }
+
+    public Button getRoll() {
+        return roll;
+    }
+
+    public Image getDie1() {
+        return die1;
+    }
+
+    public Image getDie2() {
+        return die2;
+    }
+
+    public Image getDie3() {
+        return die3;
+    }
+
+    public Image getDie4() {
+        return die4;
+    }
+
+    public Image getDie5() {
+        return die5;
+    }
+
+    public Image getDie6() {
+        return die6;
+    }
+
+    public ImageView getDiceFoto() {
+        return diceFoto;
     }
 }
