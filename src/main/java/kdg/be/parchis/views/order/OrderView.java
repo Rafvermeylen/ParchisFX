c6848316a98ad6d2917895dbe58b77a23405c0ad
Daniel Gomes <rui.gomesvieira@student.kdg.be>
2023-02-12 20:14:50 +0100

    Changed background image on orderview.

--- src/main/java/kdg/be/parchis/views/order/OrderView.java
+++ src/main/java/kdg/be/parchis/views/order/OrderView.java
@@ -38,7 +38,7 @@ public class OrderView extends BorderPane {
         this.layoutNodes();
     }
     private void initialiseNodes() throws FileNotFoundException {
-        background = new Image(new FileInputStream("resources\\backgrounds\\Background.png"));
+        background = new Image(new FileInputStream("resources\\graphics\\game\\background_ingame.png"));
         player1Name = new Label(setup.getPlayers().get(0).getName());
         player2Name = new Label(setup.getPlayers().get(1).getName());
         player3Name = new Label(setup.getPlayers().get(2).getName());
@@ -60,12 +60,10 @@ public class OrderView extends BorderPane {
         names.getChildren().addAll(player1Name, player2Name, player3Name, player4Name);
         names.setAlignment(Pos.CENTER);
         HBox layout = new HBox(30);
-        layout.getChildren().addAll(names, roll);
+        layout.getChildren().addAll(names, roll,diceFoto);
         layout.setAlignment(Pos.CENTER);
-        this.setTop(diceFoto);
         this.setCenter(layout);
         this.setBottom(back);
-        BorderPane.setAlignment(diceFoto, Pos.TOP_CENTER);
         BorderPane.setMargin(diceFoto, new Insets(30));
         BorderPane.setAlignment(layout, Pos.CENTER);
         this.setBackground(new Background(new BackgroundImage(background, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
