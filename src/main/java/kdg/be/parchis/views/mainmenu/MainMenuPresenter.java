5e092746582cf784c203e97ce6b9ca71bba110bf
Dany <danygomesvieira@hotmail.com>
2023-02-16 07:21:59 +0100

    Added fullscreen function by pressing f

--- src/main/java/kdg/be/parchis/views/mainmenu/MainMenuPresenter.java
+++ src/main/java/kdg/be/parchis/views/mainmenu/MainMenuPresenter.java
@@ -3,8 +3,18 @@ package kdg.be.parchis.views.mainmenu;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.event.EventType;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.media.AudioClip;
 import javafx.scene.media.Media;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import javafx.stage.WindowEvent;
 import kdg.be.parchis.views.leaderboards.LeaderboardPresenter;
 import kdg.be.parchis.views.leaderboards.LeaderboardView;
 import kdg.be.parchis.model.menu.Cheats;
@@ -14,14 +24,19 @@ import kdg.be.parchis.views.playerselect.PlayerSelectView;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Optional;
 
 public class MainMenuPresenter {
     private MainMenuView view;
+    private final KeyCode fullscreenKey = KeyCode.F;
+    private final KeyCode cheatKey = KeyCode.C;
+
     public MainMenuPresenter(MainMenuView view) {
         this.view = view;
         this.addEventHandlers();
         this.updateView();
     }
+
     private void addEventHandlers() {
 // Koppelt event handlers (anon. inner klassen)
 // aan de controls uit de view.
@@ -37,22 +52,13 @@ public class MainMenuPresenter {
             @Override
             public void handle(ActionEvent actionEvent) {
                 //Show if cheats have been activated or disabled
+                String path = "resources\\audio\\click.mp3";
+                Media media = new Media(new File(path).toURI().toString());
+                // Just media doesn't work, x.getSource has to be added as well.
+                AudioClip sound = new AudioClip(media.getSource());
+                sound.play();
                 Cheats.clickButton();
-                if (Cheats.getActivated()){
-                    String path = "resources\\audio\\click.mp3";
-                    Media media = new Media(new File(path).toURI().toString());
-                    // Just media doesn't work, x.getSource has to be added as well.
-                    AudioClip sound = new AudioClip(media.getSource());
-                    sound.play();
-                    view.getCheat().setText("Cheats: ON");
-                } else {
-                    String path = "resources\\audio\\click.mp3";
-                    Media media = new Media(new File(path).toURI().toString());
-                    // Just media doesn't work, x.getSource has to be added as well.
-                    AudioClip sound = new AudioClip(media.getSource());
-                    sound.play();
-                    view.getCheat().setText("Cheats: OFF");
-                }
+                updateView();
             }
         });
         view.getLeaderboards().setOnAction(new EventHandler<ActionEvent>() {
@@ -92,11 +98,42 @@ public class MainMenuPresenter {
             }
         });
     }
+
     private void updateView() {
 // Vult de view met data uit model
+        if (Cheats.getActivated()) {
+            view.getCheat().setText("Cheats: ON");
+        } else {
+            view.getCheat().setText("Cheats: OFF");
+        }
     }
-    public void addWindowEventHandlers () {
-// Window event handlers (anon. inner klassen)
-// Koppeling via view.getScene().getWindow()
+
+    public void addWindowEventHandlers() {
+        //make game fullscreen by pressing f
+        view.getScene().setOnKeyPressed(e -> {
+            if (e.getCode() == fullscreenKey) {
+                Scene scene = view.getScene();
+                if (scene != null) {
+                    Stage stage = (Stage) scene.getWindow();
+                    stage.setFullScreen(!stage.isFullScreen());
+                }
+            }
+        });
+        //confirm window alert
+        /*
+        Window window = view.getScene().getWindow();
+        window.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent windowEvent) {
+                Alert areYouSure = new Alert(Alert.AlertType.CONFIRMATION);
+                areYouSure.setHeaderText("Quiting already?");
+                areYouSure.setContentText("Are you sure you want to quit?");
+                Optional<ButtonType> keuze = areYouSure.showAndWait();
+                if (keuze.get().getText().equalsIgnoreCase("CANCEL")) {
+                    windowEvent.consume();
+                }
+            }
+        });
+         */
     }
 }
