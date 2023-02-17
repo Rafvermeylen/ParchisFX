a35d97b260d2ba236b94a4051f4de0f1696bf60d
Raf Vermeylen <raf.vermeylen@student.kdg.be>
2023-02-17 11:36:29 +0100

    optimised SoundClass.java.

--- src/main/java/kdg/be/parchis/model/musicLogic/SoundClass.java
+++ src/main/java/kdg/be/parchis/model/musicLogic/SoundClass.java
@@ -6,21 +6,20 @@ import javafx.scene.media.Media;
 import java.io.File;
 
 public class SoundClass {
+    private static String path = "resources\\audio\\sfx\\";
 
-    public static void playClick(){
-        String path = "resources\\audio\\sfx\\click.mp3";
-        Media media = new Media(new File(path).toURI().toString());
+    private static void initSound(String path, String name) {
+        Media media = new Media(new File(path + name).toURI().toString());
         // Just media doesn't work, x.getSource has to be added as well.
         AudioClip sound = new AudioClip(media.getSource());
         sound.play();
+    }
+
+    public static void playClick(){
+        initSound(path, "click.mp3");
     }
 
     public static void playRoll(){
-        String path = "resources\\audio\\sfx\\roll.wav";
-        Media media = new Media(new File(path).toURI().toString());
-        // Just media doesn't work, x.getSource has to be added as well.
-        AudioClip sound = new AudioClip(media.getSource());
-        sound.play();
+        initSound(path, "roll.wav");
     }
-
 }
