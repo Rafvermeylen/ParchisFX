e01e292df8efa32ae37a81d00818f514299434e7
Raf Vermeylen <raf.vermeylen@student.kdg.be>
2023-02-19 14:55:51 +0100

    Tried to make all the songs play. Works with 2 songs.

--- src/main/java/kdg/be/parchis/model/musicLogic/MainMusic.java
+++ src/main/java/kdg/be/parchis/model/musicLogic/MainMusic.java
@@ -4,16 +4,21 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 import java.io.File;
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MainMusic {
     private static Media menu_music;
 
+    private static int currentSongIndex = 0;
+
     private static MediaPlayer mediaPlayer;
 
-    private static Media[] songs = new Media[]{
+    private static List<Media> songs = new ArrayList<>();
 
-            new Media(new File("resources\\audio\\music\\Legend_of_Skullz.mp3").toURI().toString()),
+    /*
+    new Media(new File("resources\\audio\\music\\Legend_of_Skullz.mp3").toURI().toString()),
             new Media(new File("resources\\audio\\music\\Jazz_Soul.mp3").toURI().toString()),
             new Media(new File("resources\\audio\\music\\Hairy_Monkey_Balls.mp3").toURI().toString()),
             new Media(new File("resources\\audio\\music\\Guilty.mp3").toURI().toString()),
@@ -24,8 +29,8 @@ public class MainMusic {
             new Media(new File("resources\\audio\\music\\Noire_Clarinet.mp3").toURI().toString()),
             new Media(new File("resources\\audio\\music\\Pleasure.mp3").toURI().toString()),
             new Media(new File("resources\\audio\\music\\Temptation.mp3").toURI().toString()),
-    };
 
+     */
     public static void playMenuMusic() {
         menu_music = new Media(new File("resources\\audio\\music\\menu_music.mp3").toURI().toString());
         mediaPlayer = new MediaPlayer(menu_music);
@@ -34,21 +39,54 @@ public class MainMusic {
     }
 
     public static void playGameMusic() {
-        Random rn = new Random();
-        int choice = rn.nextInt(0, songs.length);
-        mediaPlayer = new MediaPlayer(songs[choice]);
-        mediaPlayer.play();
-        mediaPlayer.setAutoPlay(true);
-        /*
+        // https://techwithmaddy.com/java-8-lambda-expression
+        // https://techwithmaddy.com/java-method-reference
+
+        songs.add(new Media(new File("resources\\audio\\music\\Jazz_Soul.mp3").toURI().toString()));
+        songs.add(new Media(new File("resources\\audio\\music\\Hairy_Monkey_Balls.mp3").toURI().toString()));
+        songs.add(new Media(new File("resources\\audio\\music\\Guilty.mp3").toURI().toString()));
+        songs.add(new Media(new File("resources\\audio\\music\\Legend_of_Skullz.mp3").toURI().toString()));
+        songs.add(new Media(new File("resources\\audio\\music\\J.J..mp3").toURI().toString()));
+        songs.add(new Media(new File("resources\\audio\\music\\Jazz_Crossing.mp3").toURI().toString()));
+        songs.add(new Media(new File("resources\\audio\\music\\Jazz_In_Paris.mp3").toURI().toString()));
+        songs.add(new Media(new File("resources\\audio\\music\\Minor.mp3").toURI().toString()));
+        songs.add(new Media(new File("resources\\audio\\music\\Noire_Clarinet.mp3").toURI().toString()));
+        songs.add(new Media(new File("resources\\audio\\music\\Pleasure.mp3").toURI().toString()));
+        songs.add(new Media(new File("resources\\audio\\music\\Temptation.mp3").toURI().toString()));
+
+        Collections.shuffle(songs);
+
+        mediaPlayer = new MediaPlayer(songs.get(currentSongIndex));
+
+        // A lambda expression is a function that doesn’t belong to any class.
+        // For my understanding, this will happen if the song has ended. It will keep looping until list is completed.
+        // If list is completed, currentSongIndex will be set back to 0, restarting the process.
         do {
-            mediaPlayer = new MediaPlayer(songs[choice]);
-            mediaPlayer.play();
-        } while (songs.length != 0);
+            mediaPlayer.setOnEndOfMedia(() -> {
+                // Increment the index to play the next song
+                currentSongIndex++;
 
-        if (mediaPlayer.getOnEndOfMedia() == true) {
+                // Create a new MediaPlayer for the next song and play it
+                mediaPlayer.dispose();
+                mediaPlayer = new MediaPlayer(songs.get(currentSongIndex));
 
+                // :: operator refers to a method reference ==> simplified way
+                // of writing a lambda expression in order to call a method.
+                //mediaPlayer.setOnEndOfMedia(mediaPlayer::stop);
+                mediaPlayer.play();
+            });
+        } while (currentSongIndex >= songs.size());
+
+        if (currentSongIndex >= songs.size()) {
+            // If the index goes above 0, set it back to 0 to loop back to the beginning
+            currentSongIndex = 0;
         }
-        */
+/*
+        mediaPlayer.setOnStopped(() -> {
+            mediaPlayer.dispose();
+        });
+ */
+        mediaPlayer.play();
     }
 
     public static void stopMusic() {
