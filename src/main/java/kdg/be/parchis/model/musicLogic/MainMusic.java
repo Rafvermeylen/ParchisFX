cedc4419d72902890e54486dea692cf0e76cd189
Raf Vermeylen <raf.vermeylen@student.kdg.be>
2023-02-27 15:07:36 +0100

    Fixed music

--- src/main/java/kdg/be/parchis/model/musicLogic/MainMusic.java
+++ src/main/java/kdg/be/parchis/model/musicLogic/MainMusic.java
@@ -10,24 +10,9 @@ import java.util.List;
 
 public class MainMusic {
     private static Media menu_music;
-    private static int currentIndex;
     private static MediaPlayer mediaPlayer;
     private static List<Media> songs = new ArrayList<>();
 
-    /*
-    new Media(new File("resources\\audio\\music\\Legend_of_Skullz.mp3").toURI().toString()),
-            new Media(new File("resources\\audio\\music\\Jazz_Soul.mp3").toURI().toString()),
-            new Media(new File("resources\\audio\\music\\Hairy_Monkey_Balls.mp3").toURI().toString()),
-            new Media(new File("resources\\audio\\music\\Guilty.mp3").toURI().toString()),
-            new Media(new File("resources\\audio\\music\\J.J..mp3").toURI().toString()),
-            new Media(new File("resources\\audio\\music\\Jazz_Crossing.mp3").toURI().toString()),
-            new Media(new File("resources\\audio\\music\\Jazz_In_Paris.mp3").toURI().toString()),
-            new Media(new File("resources\\audio\\music\\Minor.mp3").toURI().toString()),
-            new Media(new File("resources\\audio\\music\\Noire_Clarinet.mp3").toURI().toString()),
-            new Media(new File("resources\\audio\\music\\Pleasure.mp3").toURI().toString()),
-            new Media(new File("resources\\audio\\music\\Temptation.mp3").toURI().toString()),
-
-     */
     public static void playMenuMusic() {
         menu_music = new Media(new File("resources\\audio\\music\\menu_music.mp3").toURI().toString());
         mediaPlayer = new MediaPlayer(menu_music);
@@ -35,13 +20,8 @@ public class MainMusic {
         mediaPlayer.play();
     }
 
-    private static void songsAdded() {
-
-    }
-
     public static void playGameMusic() {
         // https://techwithmaddy.com/java-8-lambda-expression
-        // https://techwithmaddy.com/java-method-reference
 
         songs.add(new Media(new File("resources\\audio\\music\\Jazz_Soul.mp3").toURI().toString()));
         songs.add(new Media(new File("resources\\audio\\music\\Hairy_Monkey_Balls.mp3").toURI().toString()));
@@ -63,45 +43,6 @@ public class MainMusic {
             mediaPlayer.dispose();
             playGameMusic();
         });
-        /*
-
-        // Set up the onEndOfMedia event to play the next song when the current one finishes
-        mediaPlayer.setOnEndOfMedia(() -> {
-            // Get the index of the current song
-            currentIndex = songs.indexOf(mediaPlayer.getMedia());
-
-            // If there's another song in the list, play it
-            if (currentIndex < songs.size() - 1) {
-                mediaPlayer = new MediaPlayer(songs.get(currentIndex + 1));
-                mediaPlayer.play();
-            }
-            // Otherwise, loop back to the beginning and play the first song
-            else {
-                mediaPlayer.dispose();
-                mediaPlayer = new MediaPlayer(songs.get(0));
-                mediaPlayer.play();
-            }
-        });
-        mediaPlayer.setOnEndOfMedia(() -> {
-            // Get the index of the current song
-            currentIndex = songs.indexOf(mediaPlayer.getMedia());
-
-            // If there's another song in the list, play it
-            if (currentIndex < songs.size() - 1) {
-                mediaPlayer.dispose();
-                mediaPlayer = new MediaPlayer(songs.get(currentIndex + 1));
-                mediaPlayer.play();
-            }
-            // Otherwise, loop back to the beginning and play the first song
-            else {
-                mediaPlayer.dispose();
-                mediaPlayer = new MediaPlayer(songs.get(0));
-                mediaPlayer.play();
-            }
-        });
-    }
-
-         */
     }
 
     public static void stopMusic() {
